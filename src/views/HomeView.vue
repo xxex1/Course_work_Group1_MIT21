b2e234a2be627ef772a2cf80bd72accc3415db07
Dmytro <dimasahah15@gmail.com>
2024-12-04 17:53:25 +0200

    micro formating

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,8 +4,8 @@
 </template>
 
 <script setup>
-import Header from '@/components/Header.vue';
-import HomePageBox from '@/components/HomePageBox.vue';
+  import Header from '@/components/Header.vue';
+  import HomePageBox from '@/components/HomePageBox.vue';
 </script>
 
 <style scoped>
--- src/views/PriceConverterView.vue
+++ src/views/PriceConverterView.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script setup>
-import Header from '@/components/Header.vue';
+  import Header from '@/components/Header.vue';
 </script>
 
 <style scoped>
