53fada6f80d4c2570f617e58d9f5ebdfa0c75ce7
Dmytro <dimasahah15@gmail.com>
2024-12-04 17:08:57 +0200

    update HomeView: add CentralBox

---
+++ src/components/CentralBox.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="center-container">
+    
+    <img src="../assets/coins1.svg">
+  </div>
+</template>
+
+<script setup>
+const imageSrc = '../assets/coins1.svg'; // Путь к вашему изображению
+</script>
+
+<style scoped>
+.center-container {
+  display: flex;
+  justify-content: right; /* Горизонтальное центрирование */
+  align-items: center; /* Вертикальное центрирование */
+  height: 100vh;
+}
+
+img {
+  max-width: 100%;
+  max-height: 100%;
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
   <Header />
+  <CentralBox />
 </template>
 
 <script setup>
 import Header from '@/components/Header.vue';
+import CentralBox from '@/components/CentralBox.vue';
 </script>
 
 <style scoped>
