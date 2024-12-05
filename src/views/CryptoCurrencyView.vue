0a298a18aad7706b745cefa9efdf439297a57a59
Slava <slavabohdan1703@gmail.com>
2024-12-05 13:05:37 +0200

    adding design to currency page, changing search icon in home page

--- src/views/CryptoCurrencyView.vue
+++ src/views/CryptoCurrencyView.vue
@@ -1,9 +1,11 @@
 <template>
   <Header />
+  <CurrencyPage />
 </template>
 
 <script setup>
   import Header from '@/components/Header.vue';
+  import CurrencyPage from '@/components/CurrencyPage.vue';
 </script>
 
 <style scoped>
